8040bceda8e19d1630811470ae878a933811324c
weiguofeng <531940221@qq.com>
2020-12-10 00:27:27 +0800

    提交构建脚本main

--- deploy.sh
+++ deploy.sh
@@ -20,8 +20,9 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:weiguofeng/docs.git master:gh-pages
+git push -f git@github.com:weiguofeng/docs.git main:gh-pages
+
 # 比如      git@github.com:weiguofeng/docs.git
-    # git push -f git@github.com:weiguofeng/docs.git master:gh-pages
+# git push -f git@github.com:weiguofeng/docs.git main:gh-pages
 
 cd -
