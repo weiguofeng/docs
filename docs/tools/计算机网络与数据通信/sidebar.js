600eb3071e78687926bf257e1f067985f9f79579
weiguofeng <531940221@qq.com>
2020-12-22 01:21:15 +0800

    添加 Guide笔记3-1

--- docs/tools/计算机网络与数据通信/sidebar.js
+++ docs/tools/计算机网络与数据通信/sidebar.js
@@ -6,13 +6,13 @@ module.exports = [
         title:'计算机网络与数据通信',
         collapsable: true,
         children:[
-            '/计算机网络与数据通信/notes/dubbo',
-            '/计算机网络与数据通信/notes/HTTPS中的TLS',
-            '/计算机网络与数据通信/notes/message-queue',
-            '/计算机网络与数据通信/notes/rabbitmq',
-            '/计算机网络与数据通信/notes/干货：计算机网络知识总结',
-            '/计算机网络与数据通信/notes/数据通信(RESTful、RPC、消息队列)',
-            '/计算机网络与数据通信/notes/计算机网络',
+            '/tools/计算机网络与数据通信/notes/dubbo',
+            '/tools/计算机网络与数据通信/notes/HTTPS中的TLS',
+            '/tools/计算机网络与数据通信/notes/message-queue',
+            '/tools/计算机网络与数据通信/notes/rabbitmq',
+            '/tools/计算机网络与数据通信/notes/干货：计算机网络知识总结',
+            '/tools/计算机网络与数据通信/notes/数据通信(RESTful、RPC、消息队列)',
+            '/tools/计算机网络与数据通信/notes/计算机网络',
         ]
     }
 
