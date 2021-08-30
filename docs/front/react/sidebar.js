f66d22456bcf24ae2da222ba4abb069d4ee9b159
weiguofeng <531940221@qq.com>
2021-08-30 10:21:00 +0800

    react笔记上传

--- docs/front/react/sidebar.js
+++ docs/front/react/sidebar.js
@@ -35,6 +35,62 @@ module.exports = [
             '/front/react/react组件进阶/6.props的深入',
             '/front/react/react组件进阶/7.组件的生命周期',
             '/front/react/react组件进阶/8.组件的生命周期2',
+            '/front/react/react组件进阶/9.组件的生命周期3',
+            '/front/react/react组件进阶/10.render-props和高阶组件',
+            '/front/react/react组件进阶/11.高阶组件',
+            '/front/react/react组件进阶/12.高阶组件2',
+        ]
+    },
+    {
+        title:'react路由',
+        collapsable: true,
+        children:[
+            '/front/react/react路由/1.react路由了解',
+            '/front/react/react路由/2.路由的基本使用',
+            '/front/react/react路由/3.路由的基本使用2',
+            '/front/react/react路由/4.路由的执行过程',
+            '/front/react/react路由/5.编程式导航',
+            '/front/react/react路由/6.默认路由',
+            '/front/react/react路由/7.匹配模式',
+            '/front/react/react路由/8.匹配模式（精确匹配）',
+        ]
+    },
+    {
+        title:'react原理揭秘',
+        collapsable: true,
+        children:[
+            '/front/react/react原理揭秘/1.React原理揭秘介绍',
+            '/front/react/react原理揭秘/2.setState方法的说明',
+            '/front/react/react原理揭秘/3.JSX转化的过程',
+            '/front/react/react原理揭秘/4.组件更新机制',
+            '/front/react/react原理揭秘/5.组件性能优化',
+            '/front/react/react原理揭秘/6虚拟DOM和Diff算法',
+        ]
+    },
+    {
+        title:'好客租房移动Web1',
+        collapsable: true,
+        children:[
+            '/front/react/好客租房移动Web1/1.好客租房移动web项目介绍',
+            '/front/react/好客租房移动Web1/2.初始化项目',
+            '/front/react/好客租房移动Web1/3.组件库antd-mobile',
+            '/front/react/好客租房移动Web1/4.配置基础路由',
+            '/front/react/好客租房移动Web1/5.外观样式设置',
+            '/front/react/好客租房移动Web1/6.俩种布局页面',
+            '/front/react/好客租房移动Web1/7.嵌套路由',
+            '/front/react/好客租房移动Web1/8.实现tabBar',
+            '/front/react/好客租房移动Web1/9.首页路由处理',
+            '/front/react/好客租房移动Web1/10.轮播图',
+            '/front/react/好客租房移动Web1/11.导航菜单',
+            '/front/react/好客租房移动Web1/12.轮播图Bug分析修复',
+            '/front/react/好客租房移动Web1/13.TabBar菜单高亮Bug分析修复',
+            '/front/react/好客租房移动Web1/14.在脚手架中使用sass',
+            '/front/react/好客租房移动Web1/15.租房小组的业务介绍',
+            '/front/react/好客租房移动Web1/16.租房小组结构和样式',
+            '/front/react/好客租房移动Web1/17.顶部导航',
+            '/front/react/好客租房移动Web1/18.H5中地理位置的API',
+            '/front/react/好客租房移动Web1/19.百度地图API',
+            '/front/react/好客租房移动Web1/20.申请百度账户和秘钥',
         ]
     },
 
