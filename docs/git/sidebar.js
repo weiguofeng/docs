a4c19e5d261481a704b85f97658298d08904c7c5
weiguofeng <531940221@qq.com>
2021-12-31 16:08:56 +0800

    笔记的整理和侧边栏的配置

--- docs/git/sidebar.js
+++ docs/git/sidebar.js
@@ -3,24 +3,38 @@ module.exports = [
         title:'首页'
     },
     {
-        title:'基本介绍',
-        collapsable: true,
-        children:[
-            '/git/持续集成CI/1.基本介绍Git持续集成'
-        ]
-    },
-    {
         title:'Git基本操作',
         collapsable: true,
         children:[
-            '/git/持续集成CI/1.基本介绍Git持续集成'
+            '/git/git-gitlab基本操作/1.基本介绍Git持续集成',
+            '/git/git-gitlab基本操作/2.持续集成基本介绍',
+            '/git/git-gitlab基本操作/3.持续交付的基本介绍',
+            '/git/git-gitlab基本操作/4.持续部署的基本介绍',
+            '/git/git-gitlab基本操作/5.持续集成实现流程',
+            '/git/git-gitlab基本操作/6.版本控制系统介绍',
+            '/git/git-gitlab基本操作/7.git基本操作-提交代码',
+            '/git/git-gitlab基本操作/8.git基本操作-修改文件名称并提交',
+            '/git/git-gitlab基本操作/9.git基本操作-如何对比文件',
+            '/git/git-gitlab基本操作/10.git基本操作-如何实现回退',
+            '/git/git-gitlab基本操作/11.git基本操作-分支原理',
+            '/git/git-gitlab基本操作/12.git基本操作-标签',
+            '/git/git-gitlab基本操作/13.git基本操作-远程仓库',
+            '/git/git-gitlab基本操作/14.gitLab远程仓库介绍',
+            '/git/git-gitlab基本操作/15.gitLab安装配置',
+            '/git/git-gitlab基本操作/16.gitLab汉化处理',
+            '/git/git-gitlab基本操作/17.gitLab用户-用户组-关系图解',
+            '/git/git-gitlab基本操作/18.gitLab用户-组-项目-配置实践',
+            '/git/git-gitlab基本操作/19.gitLab-多用户协同工作',
+            '/git/git-gitlab基本操作/20.gitLab-分支-Tag的使用',
+            '/git/git-gitlab基本操作/21.gitLab-基本运维-备份恢复'
         ]
     },
     {
-        title:'持续集成',
+        title:'Jenkins持续集成',
         collapsable: true,
         children:[
-            '/git/持续集成CI/1.基本介绍Git持续集成'
+            '/git/Jenkins持续集成/1.jenkins介绍安装',
+            '/git/Jenkins持续集成/2.jenkins插件-加速-上传安装-离线安装',
         ]
     },
 
