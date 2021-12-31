4f2cf0a6b64c3b494987c8c293877c45360eead1
weiguofeng <531940221@qq.com>
2021-12-31 21:03:43 +0800

    调整redis文件名称

--- docs/questions/talk/sidebar.js
+++ docs/questions/talk/sidebar.js
@@ -6,6 +6,7 @@ module.exports = [
         title:'闲谈',
         collapsable: true,
         children:[
+            '/questions/talk/notes/Redis新版多线程',
             '/questions/talk/notes/2018秋招',
             '/questions/talk/notes/个人阅读书籍清单',
             '/questions/talk/notes/选择技术方向都要考虑哪些因素',
